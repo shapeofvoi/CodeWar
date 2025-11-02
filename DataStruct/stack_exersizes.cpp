21b7fb199633194b7fa523ff0d0d1ea6584b4d0f
Madolche <wangyi177507403@gmail.com>
2025-11-02 11:44:57 +0800

    增加了一个新的设置文件和一个栈练习的C++代码文件

---
+++ DataStruct/stack_exersizes.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        
+    }
+};
