b1804547530076a0a1a212ab446a7375a020f616
Madolche <wangyi177507403@gmail.com>
2025-11-11 22:38:37 +0800

    71 简化路径

--- DataStruct/stack_exersizes.cpp
+++ DataStruct/stack_exersizes.cpp
@@ -2,10 +2,14 @@
 #include <vector>
 #include <unordered_map>
 #include <stack>
+#include <string>
+#include <sstream>
 
 using std::vector;
 using std::unordered_map;
 using std::stack;
+using std::string;
+using std::stringstream;
 
 class Solution {
 public:
@@ -34,5 +38,111 @@ public:
             ans.push_back(nextGreater[nums1[i]]);
         }
         return ans;
+    }
+
+    int calPoints(vector<string> & operations){
+        // 后进先出 用栈记录得分
+        vector<int> scores;
+        int sum = 0;
+
+        // 遍历操作，写入得分
+        for (string & o : operations){
+            int score = 0;
+            if (o == "D"){
+                score = scores.back() * 2;
+                scores.push_back(score);
+                sum += score;
+            }
+            else if (o == "C"){
+                sum -= scores.back();
+                scores.pop_back(); //弹出最后一个元素
+            }
+            else if (o == "+"){
+                score = scores.back() + scores[scores.size() - 2];
+                scores.push_back(score);
+                sum += score;
+            }
+            else{
+                score = std::stoi(o);
+                scores.push_back(score);
+                sum += score;
+            }
+        }
+
+        return sum;
+    }
+
+    string makeGood(string s) {
+        string res;
+        int n = s.size();
+
+        if (n <= 1)
+            return s;
+        else {
+            int i = 0;
+            while (i < n - 1){
+                if ((isupper(s[i]) && tolower(s[i]) == s[i+1]) || (islower(s[i]) && toupper(s[i]) == s[i+1])){
+                    s.erase(i, 2); // 从索引i开始，删除2个字符
+                    return makeGood(s);
+                }
+                ++i;
+            }
+            return s;
+        }
+    }
+
+    string makeGood(string s){
+        // 用字符串模拟栈 后进先出
+        string res;
+        
+        for (char c : s){
+            if (!res.empty() && abs(res.back() - c) == 32){
+                res.pop_back();
+            }
+            else {
+                res.push_back(c);
+            }
+        }
+        return res;
+    }
+
+    string clearDigits(string s) {
+        string res;
+
+        for(auto c : s) {
+            if (isdigit(c)){
+                res.pop_back();
+            }
+            else {
+                res.push_back(c);
+            }
+        }
+
+        return res;
+    }
+
+    string simplifyPath (string path) {
+        vector<string> stk;
+        string dir;
+        stringstream ss(path);
+
+        while (getline(ss, dir, '/')){ // stringsteam内部有指针，每次读取，指针会向后移动
+            if (dir == "" || dir == ".") continue;
+            if (dir == "..") {
+                if (!stk.empty()) stk.pop_back();
+            }
+            else {
+                stk.push_back(dir);
+            }
+        }
+
+        // 拼接
+        string res = "/";
+        for (size_t i = 0; i < stk.size(); ++i) {
+            res += stk[i];
+            if (i != stk.size() - 1) res += "/";
+        }
+
+        return res;
     }
 };
